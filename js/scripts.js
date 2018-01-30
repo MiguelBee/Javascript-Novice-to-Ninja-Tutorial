3c85814e83c0d86f81641d290a122465e22046c2
Miguel Bustamante <mbustamante98@hotmail.com>
2018-01-29 20:52:56 -0700

    Add DOM functions

--- js/scripts.js
+++ js/scripts.js
@@ -9,13 +9,38 @@ quiz = {
 	]
 }
 
-var score = 0 // initialize score
+//// dom references///
+var $question = document.getElementById("question");
+var $score = document.getElementById("score");
+var $feedback = document.getElementById("feedback");
+
+
+
+///view functions///
+
+function update(element, content, klass) {
+	//checks to see if element has a p child, else creates new one
+	var p  = element.firstChild || document.createElement("p");
+	// makes p's content that of arguement content
+	p.textContent = content;
+	//appends the new child into the element
+	element.appendChild(p);
+	if(klass) {
+		p.className = klass;
+	}
+}
 
 play(quiz);
 
+///function definitions///
+
 function play(quiz) {
+	var score = 0 //initialized score
+	update($score, score);
+
 	//main game loop
 	for(i=0, question, answer, max=quiz.questions.length; i <max; i++){
+		update($score, score);
 		var question = quiz.questions[i].question;
 		var answer = ask(question);
 		check(answer);
@@ -24,22 +49,26 @@ function play(quiz) {
 	gameOver();
 
 
+	// nested functions//
+
 	function ask(question) {
-		return prompt(quiz.question + question); // quiz[i][0] is the i'th question
+		update($question, quiz.question + question); // quiz[i][0] is the i'th question
+		return prompt("Enter Your Answer:");
 	}
 
 	function check(answer){
 		if(answer === quiz.questions[i].answer) {
-			alert("Correct!");
-			score ++
+			update($feedback, "Correct!", 'right');
+			score++ ;
+			update($score, score)
 		} else {
-			alert("Wrong!")
+			update($feedback, "Wrong!", "wrong");
 		}
 	}
 	
 	function gameOver(){
 		//inform the player that the game has finished and tell
 		//how many points scored
-		alert("Game over, your score is " + score + " points");
+		update($question, "Game Over, you scored " + score + " points");
 	}
 }
